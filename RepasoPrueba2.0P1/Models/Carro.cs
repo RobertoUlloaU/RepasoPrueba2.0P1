53175dde36fa183c9f7a55f589177c75d1a44c73
Roberto Alejandro Ulloa Urbina <roberto.ulloa@udla.edu.ec>
2025-04-29 03:46:17 -0500

    Agregamos los modelos Propietario y Carro

---
+++ RepasoPrueba2.0P1/Models/Carro.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RepasoPrueba2._0P1.Models
+{
+    public class Carro
+    {
+        [Key]
+        [MaxLength(7)]
+        public string Placa { get; set; }
+        public double Cilindraje { get; set; }
+        [MaxLength(50)]
+        public string Modelo { get; set; }
+        public string PropietarioIdentificacion { get; set; }
+
+        [ForeignKey("PropietarioIdentificacion")]
+        public string? Propietario {  get; set; }
+
+    }
+}
---
+++ RepasoPrueba2.0P1/Models/Propietario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RepasoPrueba2._0P1.Models
+{
+    public class Propietario
+    {
+        [Key]
+        [MaxLength(15)]
+        public int Identificacion { get; set; }
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [MaxLength(10)]
+        public string Telefono { get; set; }
+        [MaxLength(200)]
+        public string Direccion { get; set; }
+        
+    }
+}
