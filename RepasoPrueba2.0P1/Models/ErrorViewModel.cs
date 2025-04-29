59b762285e7eef5ee28ab74aba444162420cb08e
Roberto Alejandro Ulloa Urbina <roberto.ulloa@udla.edu.ec>
2025-04-29 03:31:20 -0500

    Agregar archivos de proyecto.

---
+++ RepasoPrueba2.0P1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepasoPrueba2._0P1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
